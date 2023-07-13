ff77e302eef893b833fd460bf5d5dce7a431b35c
Michael Driscoll <michael.driscoll951@gmail.com>
2023-07-12 20:49:39 -0700

    udemy6

---
+++ Udemy5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Udemy5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var number = 0; //num
+        var text = "text"; //string
+        var isTrue = true; //boolean
+        var myDouble = 2.5; //double
+        var myFloat = 2.5f; //float
+
+        Console.WriteLine(number);
+        Console.WriteLine(text);
+        Console.WriteLine(isTrue);
+        Console.WriteLine(myDouble);
+        Console.WriteLine(myFloat);
+        Console.ReadLine();
+
+
+
+    }
+    
+}
+
